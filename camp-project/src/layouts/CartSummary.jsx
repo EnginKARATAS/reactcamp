722f97cd8e4a04728984a8c1ba28c0b0da6bf4b6
Engin Karataş <enginkaratas99@gmail.com>
2021-06-17 14:11:43 +0300

    camp day 13: redux, store and more

--- camp-project/src/layouts/CartSummary.jsx
+++ camp-project/src/layouts/CartSummary.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { Dropdown } from "react";
+import { Dropdown, Label } from "react";
+import { useSelector } from 'react-redux';
 import { NavLink } from "semantic-ui-react";
+import { cartItems } from '../store/initalValues/cartItems';
 
 export default function CartSummary() {
+    const { createItems } = useSelectortor(state => state.cart)
     return (
-        <Dropdown item text='Language'>
+        <Dropdown item text='Sepetiniz'>
             <Dropdown.Menu>
-                <Dropdown.Item>Acer Leptop</Dropdown.Item>
-                <Dropdown.Item>Asus Leptop</Dropdown.Item>
-                <Dropdown.Item>Dell Leptop</Dropdown.Item>
+                <Dropdown.Menu>
+                    {
+                        cartItems.map((cartItem) = (
+                            <Dropdown.Item>
+                                {cartItem.product.productName}
+                            <Label>
+                                {cartItem.quantity}
+                            </Label>
+                            </Dropdown.Item>
+                        ))
+                    }
+                </Dropdown.Menu>
                 <Dropdown.Divider></Dropdown.Divider>
-                <Dropdown.Item as={NavLink} to="/cart">Sepete Git</Dropdown.Item>   
+                <Dropdown.Item as={NavLink} to="/cart">Sepete Git</Dropdown.Item>
             </Dropdown.Menu >
         </Dropdown>
 
---
+++ camp-project/src/store/actions/cartActions.js
@@ -0,0 +1,16 @@
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+function addToCart(product) {
+    return{
+        type : "ADD_TO_CART",
+        payload: product
+    }
+}
+
+function removeFromCart(product) {
+    return{
+        type : "ADD_TO_CART",
+        payload: product
+    }
+}
